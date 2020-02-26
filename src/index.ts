7339c11aba8f4581c4b3994718aef5c5fbd164f4
Kirk Scheibelhut <kjs@google.com>
2020-02-26 13:11:08 -0800

    Properly handle changes in metagame cutoffs/naming

--- src/index.ts
+++ src/index.ts
@@ -137,17 +137,6 @@ export const Analyses = new (class {
   }
 })();
 
-const POPULAR = [
-  'gen8ou',
-  'gen8doublesou',
-  'gen7ou',
-  'gen7doublesou',
-  'ou',
-  'doublesou',
-  'smogondoubles',
-  'randombattle',
-];
-
 export const Statistics = new (class {
   readonly URL = 'https://www.smogon.com/stats/';
 
@@ -163,9 +152,23 @@ export const Statistics = new (class {
     throw new Error('Unexpected format for index');
   }
 
-  url(date: string, format: string, weighted = true) {
-    const formatid = toID(format);
-    const rating = weighted ? (POPULAR.includes(format) ? 1825 : 1760) : 0;
+  url(date: string, format: string, weighted: number | boolean = true) {
+    let formatid = toID(format);
+    // When Gen 7 was released the naming scheme for 'current' formats was changed from
+    // 'x' => 'genNx'. formatFor will translate between the two as approriate, but there
+    // is an edge case for 2016-12 where both randombattle and gen6randombattle exist
+    if (!(date === '2016-12' && ['gen6randombattle', 'randombattle'].includes(formatid))) {
+      formatid = formatFor(formatid, date);
+    }
+
+    // If we've been given a weight then we use that, otherwise we use weightFor to
+    // figure out what the highest weight cutoff for the format was (usually 1760 or 1825)
+    // prettier-ignore
+    const rating = weighted
+      ? typeof weighted === 'number' ? weighted
+      : weightFor(formatid, date)
+      : 0;
+
     return `${Statistics.URL}${date}/chaos/${formatid}-${rating}.json`;
   }
 
@@ -177,3 +180,48 @@ export const Statistics = new (class {
     return Statistics.parse(raw);
   }
 })();
+
+// prettier-ignore
+const POPULAR = [
+  'gen8ou', 'gen8doublesou', 'gen7ou', 'gen7doublesou',
+  'ou', 'doublesou', 'smogondoubles', 'randombattle',
+];
+
+function weightFor(format: ID, date: string) {
+  // gen7doublesu ou and smogondoublessuspecttest have used different weights over the years
+  if (format === 'gen7doublesou' && date < '2017-02') return 1760;
+  if (format === 'smogondoublessuspecttest' && date === '2015-04') return 1825;
+  // Otherwise, formats deemed 'popular' are assigned higher weight. Note that legacy format
+  // notation is signficant here: gen6ou was only 'popular' while it was still called 'ou'
+  return POPULAR.includes(format) ? 1825 : 1760;
+}
+
+// Metagames which continued to be played after gen6, transitioning from a bare unqualified
+// name to a 'gen6'-qualified one. Most migrated over on 2017-07, though the LATE metagames
+// below were only given qualification from 2018 and onward.
+// prettier-ignore
+const CONTINUED = new Set([
+  '1v1', 'anythinggoes', 'battlespotdoubles', 'battlespotsingles', 'battlespottriples',
+  'cap', 'lc', 'monotype', 'nu', 'ou', 'pu', 'randombattle', 'ru', 'ubers', 'uu',
+  'balancedhackmons', 'doublesou', 'doublesuu', 'battlefactory', 'mixandmega', 'vgc2016',
+]);
+const LATE = ['1v1', 'cap', 'monotype', 'balancedhackmons', 'mixandmega'];
+
+function formatFor(format: ID, date: string) {
+  const m = format.match(/gen(\d)(.*)/);
+  // Return if we've been given a format with the standard notation and its not Gen 6
+  if (m && m[1] !== '6') return format as ID;
+  // Return the unqualified metagame if the format starts with 'gen6' but has been discontinued
+  if (m && !CONTINUED.has(m[2])) return m[2] as ID;
+  if (m) {
+    // If the format is 'gen6'-qualified but the date requested is before the standard 2017-06/07
+    // migration (or was a late-migrating metagame and before 2017-12/2018-01), remove the qualifier
+    return date < '2017-07' || (date < '2018-01' && !LATE.includes(m[2])) ? (m[2] as ID) : format;
+  } else {
+    // If the format unqualified but the date requested is after the standard 2017-06/07 migration
+    //(or was a late-migrating metagame and after 2017-12/2018-01), add the 'gen6'-qualifier
+    return date > '2017-12' || (date > '2017-06' && !LATE.includes(format))
+      ? (`gen6${format}` as ID)
+      : format;
+  }
+}
