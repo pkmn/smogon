6e7cbfda5646bcea20ca358b8d3174e2a47d51f7
Kirk Scheibelhut <kjs@scheibo.com>
2020-10-31 09:21:00 -0700

    Condense configs into package.json

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  coverageDirectory: 'coverage',
-  coveragePathIgnorePatterns: ['/node_modules/', '/dist/', '/src/fixtures/' ]
-};
