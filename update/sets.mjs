4b814994ffaba160a8139ebdaf5c17d3538b71b9
Kirk Scheibelhut <kjs@scheibo.com>
2023-12-29 12:43:19 -0500

    vgc regulation f support

--- update/sets.mjs
+++ update/sets.mjs
@@ -67,7 +67,7 @@ const FORMATS = {
   vgc11: 'vgc2011', vgc12: 'vgc2012', vgc14: 'vgc2014', vgc15: 'vgc2015', vgc16: 'vgc2016',
   vgc17: 'vgc2017', vgc18: 'vgc2018', vgc19: 'vgc2019', vgc20: 'vgc2020', vgc21: 'vgc2021',
   vgc22: 'vgc2022', vgc23series1: 'vgc2023', vgc23series2: 'vgc2023', vgc23series3: 'vgc2023',
-  vgc23series4: 'vgc2023', vgc24regulatione: 'vgc2023',
+  vgc23series4: 'vgc2023', vgc24regulatione: 'vgc2023', vgc24regulationf: 'vgc2024',
   // RBwhY?
   nintendocup1998: 'nintendocup1998', nintendocup1999: 'nintendocup1999', lclevel100: 'lclevel100',
   petitcup: 'petitcup', pikacup: 'pikacup', monotype: 'monotype', pu: 'pu', lc: 'lc', '1v1': '1v1',
