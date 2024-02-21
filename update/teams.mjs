1b59ad14794cbf6f2d0c025b389eb42a4d68f670
Kirk Scheibelhut <kjs@scheibo.com>
2024-02-20 19:43:09 -0800

    teams

--- update/teams.mjs
+++ update/teams.mjs
@@ -56,7 +56,7 @@ const GUEST = /Guest \d+/;
 const IGNORE = [
   // Multiple formats within a single post
   'https://www.smogon.com/forums/posts/6431094/',
-  // Muiltiple updates in one post all Within spoilers
+  // Multiple updates in one post all within spoilers
   'https://www.smogon.com/forums/threads/3725988/'
 ];
 
