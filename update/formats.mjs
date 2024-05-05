0dce701526a7292c4a2a11f05519eceaf3048218
Kirk Scheibelhut <kjs@scheibo.com>
2024-05-04 17:53:40 -0700

    update formats

--- update/formats.mjs
+++ update/formats.mjs
@@ -13,8 +13,8 @@ import stringify from 'json-stringify-pretty-compact';
 
 import {Dex} from '@pkmn/sim';
 
-import NAMES from '../data/formats/index.json' assert {type: 'json'};
-import FORMATS from '../smogon/latest.json' assert {type: 'json'};
+import NAMES from '../data/formats/index.json' with {type: 'json'};
+import FORMATS from '../smogon/latest.json' with {type: 'json'};
 
 const IGNORE = new Set([
   'gen8tiershiftcamomonsobtainable',
