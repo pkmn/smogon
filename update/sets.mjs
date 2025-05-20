1dc5625b0209408004b2d01bc55842ff5e9b0280
Kirk Scheibelhut <kjs@scheibo.com>
2025-05-20 10:00:14 -0700

    fix VGC 2025 format

--- update/sets.mjs
+++ update/sets.mjs
@@ -72,6 +72,7 @@ const FORMATS = {
   vgc22: 'vgc2022', vgc23series1: 'vgc2023', vgc23series2: 'vgc2023', vgc23series3: 'vgc2023',
   vgc23series4: 'vgc2023', vgc24regulatione: 'vgc2023', vgc24regulationf: 'vgc2024',
   vgc24regf: 'vgc2024', vgc24regg: 'vgc2024', vgc: 'vgc2024', vgc24regulationg: 'vgc2024',
+  vgc25regulationi: 'vgc2025',
   // RBwhY?
   nintendocup1997: 'nc1997', nintendocup1998: 'nc1998', nintendocup1999: 'nc1999',
   lclevel100: 'lclevel100', petitcup: 'petitcup', pikacup: 'pikacup', monotype: 'monotype',
